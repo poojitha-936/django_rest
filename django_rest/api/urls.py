35eb9f6a450a8d7c03209aecc643a1a1311fac43
poojitha-936 <karrepoojitha123@gmail.com>
2025-02-05 05:29:04 -0800

    Add files via upload

--- django_rest/api/urls.py
+++ django_rest/api/urls.py
@@ -5,5 +5,6 @@ urlpatterns =[
     path('students/', views.studentsView),
     # here url will handle the get, put request all at one
     path('students/<int:pk>', views.studentDetailView),
-    # path('employees/', views.Employee.as_view()),
+    path('employees/', views.Employees.as_view()),
+    path('employees/<int:pk>/', views.EmployeeDetail.as_view()),
 ]
