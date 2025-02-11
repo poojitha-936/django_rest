8c61a47a05d678ebe61f50ffde2b3d6404c74f65
poojitha-936 <karrepoojitha123@gmail.com>
2025-02-11 02:16:53 -0800

    Update urls.py

--- django_rest/api/urls.py
+++ django_rest/api/urls.py
@@ -13,7 +13,7 @@ urlpatterns =[
     path('employees/', views.Employees.as_view()),
     path('employees/<int:pk>/', views.EmployeeDetail.as_view()),
 
-    # path('', include(router.urls)),
+    path('', include(router.urls)),
  
 
 ]
